b2f5f7f2c25fd4658b7bdc10947436205961f551
Jorge Francisco Aguirre Concha <jorgef.aguir@gmail.com>
2024-09-26 23:56:27 -0300

    added biome to have some linting

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import path from 'path';
 import scraperRoutes from './routes/scraper-routes';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
